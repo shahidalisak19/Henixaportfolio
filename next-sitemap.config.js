e95e8aee35612b2272e91d1628af419155f8cb9a
shahidalisak19 <shahidalisak19@gmail.com>
2026-01-17 14:03:14 +0500

    ddd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
+// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://henixaportfolio.vercel.app',
+  siteUrl: 'https://henixaportfolio.vercel.app', // Your real domain
   generateRobotsTxt: true,
-  sitemapSize: 5000,
-  changefreq: 'weekly',
-  priority: 0.7,
-  outDir: './public', // Generate sitemap in public folder
+  sitemapSize: 50000,       // Large number to avoid multiple sitemaps
+  outDir: './public',       // Ensure sitemap is generated in public folder
 }
